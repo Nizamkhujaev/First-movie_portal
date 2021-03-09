ae6a61c066232bef4e44ef4746d313f89017a438
shon_parker <nizamkhujaev@gmail.com>
2021-03-10 00:15:22 +0500

    Updated version number

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import './assets/styles/app.scss'
+
+import HomePage from './pages/homePage';
+import PopularsPage from './pages/popularsPage';
+import MoviesPage from './pages/moviesPage';
+import SingleMoviePage from './pages/singleMovie';
+import Header from './containers/header/header'
+import TvShows from './pages/TvShows';
+import SingleTv from './containers/singleTv/singleTv';
+import AdviseActor from './containers/adviseActor/adviseActor';
+import Footer from './containers/footer/footer';
+import SearchResults from './pages/searchResults'
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+}
+ from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+
+        <Header/>
+
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route exact path="/popular" component={PopularsPage} />
+          <Route exact path="/movies" component={MoviesPage} />
+          <Route exact path="/movie/:id" component={SingleMoviePage} />
+          <Route exact path="/tv-show" component={TvShows} />
+          <Route exact path="/tv/:id" component={SingleTv}/>
+          <Route exact path="/movie/actor/:id" component={AdviseActor} />
+          <Route exact path="/search/:searchQuery" component={SearchResults} />
+        </Switch>
+
+        <Footer/>
+
+      </div>  
+    </Router>
   );
 }
 
